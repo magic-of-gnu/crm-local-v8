e5877ffc1a5377b1920655baaa242f878d536690
My Name <you@example.com>
2023-03-11 13:23:44 +0600

    added GetAll

--- server/internal/services/centres.go
+++ server/internal/services/centres.go
@@ -1,1 +1,20 @@
 package services
+
+import (
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/repos"
+)
+
+type centresService struct {
+	CentresRepo repos.CentresRepo
+}
+
+func NewCentresService(centresRepo repos.CentresRepo) *centresService {
+	return &centresService{
+		CentresRepo: centresRepo,
+	}
+}
+
+func (ss *centresService) GetAll() ([]models.Centre, error) {
+	return ss.CentresRepo.GetAll()
+}
