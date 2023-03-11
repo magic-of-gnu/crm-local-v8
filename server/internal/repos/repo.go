a62d34e56e604b564d23343f13f45d6a497b5fb1
My Name <you@example.com>
2023-03-11 14:57:10 +0600

    added CreateOne method

--- server/internal/repos/repo.go
+++ server/internal/repos/repo.go
@@ -1,7 +1,12 @@
 package repos
 
-import "github.com/magic-of-gnu/crm-local-v8/server/internal/models"
+import (
+	"time"
+
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
+)
 
 type CentresRepo interface {
 	GetAll() ([]models.Centre, error)
+	CreateOne(id int, name, description string, created_at, updated_at time.Time) (*models.Centre, error)
 }
