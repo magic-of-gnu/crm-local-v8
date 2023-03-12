99a308ccfaabcba6bdcf9554a2d2597abbcaeed2
My Name <you@example.com>
2023-03-12 15:27:33 +0600

    replaced int ids to uuid ids

--- server/migrations/20230302121331_populate_attendance_values_payment_statuses_rooms_centres_tables.postgres.up.sql
+++ server/migrations/20230302121331_populate_attendance_values_payment_statuses_rooms_centres_tables.postgres.up.sql
@@ -1,25 +1,24 @@
 INSERT INTO public.attendance_values (id,value,name,description,created_at,updated_at) VALUES
-	 (0, 1,'visited','Visited the lesson',now(),now()),
-	 (1, 2,'late','Visited but came late',now(),now()),
-	 (2, 3,'excused','Absent with an excuse',now(),now()),
-	 (3, 4,'absent','Absent with no excuse',now(),now()),
-	 (4, 5,'class did not start','Absent with no excuse',now(),now());
-
+	 ('856bd4bc-cd52-4ca9-baee-10b1e78c1cdb', 1,'visited','Visited the lesson',now(),now()),
+	 ('fc850071-50d7-4c63-85a2-87e9769f6f1f', 2,'late','Visited but came late',now(),now()),
+	 ('4195f773-e32e-4c8a-a35b-67aa53421188', 3,'excused','Absent with an excuse',now(),now()),
+	 ('a0046fd8-44b9-44be-9f5d-bd5339dbab5f', 4,'absent','Absent with no excuse',now(),now()),
+	 ('8e0334c3-b2a8-464f-aaf4-27b06cbbdc32', 5,'class did not start','Absent with no excuse',now(),now());
 
 
 INSERT INTO public.payment_statuses (id, name,description,created_at,updated_at) VALUES
-	 (0, 'Paid','Payment has been made',now(), now()),
-	 (1, 'Waiting For Payment','Invoice has been sent, wating for payment',now(), now()),
-	 (2, 'Missed Payment','Missed payment, for reference Missing payment is 5 days after sending invoice',now(), now()),
-	 (3, 'Idle','When invoice has not been made',now(), now());
+	 ('bc8c944e-bcd7-4f7c-88fc-ee41733470e3', 'Paid','Payment has been made',now(), now()),
+	 ('1cb5a948-04c4-4820-9588-25cdab29f821', 'Waiting For Payment','Invoice has been sent, wating for payment',now(), now()),
+	 ('f1ec986e-4604-46cb-8e4d-cc6f21966db2', 'Missed Payment','Missed payment, for reference Missing payment is 5 days after sending invoice',now(), now()),
+	 ('f6db5964-1552-4e5c-807d-62dcac4f46f4', 'Idle','When invoice has not been made',now(), now());
 
 
 
 INSERT INTO public.centres (id,name,description,created_at,updated_at) VALUES
-	 (0,'Easy Academy','First Centre on Sauran Street','2023-02-14 00:00:00.000','2023-02-14 00:00:00.000');
+	 ('a7aa2031-6184-4a07-baa9-af14bb871a24','Easy Academy','First Centre on Sauran Street','2023-02-14 00:00:00.000','2023-02-14 00:00:00.000');
 
 INSERT INTO public.rooms (id,centre_id,name,num_seats,info,created_at,updated_at) VALUES
-	 (0, 0,'Rico','0','no additional',now(), now()),
-	 (1, 0,'Lucik','6','no additional',now(), now()),
-	 (2, 0,'Loona','6','no additional',now(), now()),
-	 (3, 0,'Kiko','6','no additional',now(), now());
+	 ('3ea97eb3-6791-46de-a9a8-738462e41d4d', 'a7aa2031-6184-4a07-baa9-af14bb871a24','Rico','0','no additional',now(), now()),
+	 ('250ac123-7f1d-4746-8512-7ae8eba80200', 'a7aa2031-6184-4a07-baa9-af14bb871a24','Lucik','6','no additional',now(), now()),
+	 ('b3a80867-fdf6-45dc-85c5-c253d6d5b5ab', 'a7aa2031-6184-4a07-baa9-af14bb871a24','Loona','6','no additional',now(), now()),
+	 ('af219e63-af0d-41cf-86e6-7ee681280359', 'a7aa2031-6184-4a07-baa9-af14bb871a24','Kiko','6','no additional',now(), now());
--- server/migrations/20230302132525_add_my_data_dump.postgres.up.sql
+++ server/migrations/20230302132525_add_my_data_dump.postgres.up.sql
@@ -1,55 +1,55 @@
 insert into employees (id, first_name, last_name, username, info, created_at, updated_at)
 values 
-(1, 'Araylim', 'Qabsitbay', 'araylim.qapsitbay@gmail.com', 'owner',now(), now()  ),
-(2, 'Umit', 'Hopeqizi', 'umit.hopeqizi@gmail.com', 'whiter than snow',now(), now()  ),
-(3, 'Fariza', 'Ferudaqizi', 'Fariza.ferudaqizi@gmail.com', '?',now(), now()  ),
-(4, 'SSS', 'HHH', 'sss.hhh@gmail.com', 'yyy',now(), now()  ),
-(5, 'b1', 'c1', 'b1.c1@gmail.com', 'deq1',now(), now()  );
+('8c8a8dd1-307d-43fa-941c-0466da9b4ab4', 'Araylim', 'Qabsitbay', 'araylim.qapsitbay@gmail.com', 'owner',now(), now()  ),
+('770ea8e3-d5ce-44cc-8ec7-45d7cced42a9', 'Umit', 'Hopeqizi', 'umit.hopeqizi@gmail.com', 'whiter than snow',now(), now()  ),
+('b0cec344-5f4c-40da-912c-442dae270810', 'Fariza', 'Ferudaqizi', 'Fariza.ferudaqizi@gmail.com', '?',now(), now()  ),
+('9b22009a-7a64-47e3-99ec-32c072f1e46e', 'SSS', 'HHH', 'sss.hhh@gmail.com', 'yyy',now(), now()  ),
+('0e6dea76-a4b9-4f91-87a0-52d80e79e565', 'b1', 'c1', 'b1.c1@gmail.com', 'deq1',now(), now()  );
 
 
 
 insert into students (id, first_name, last_name, username, created_at, updated_at)
-values (1, 'a1', 'b1', 'a1.b1@gmail.com', now(), now()  ),
-(2, 'a2', 'b2', 'a2.b2@gmail.com', now(), now()  ),
-(3, 'a3', 'b3', 'a3.b3h@gmail.com',now(), now()  ),
-(4, 'a4', 'c4', 'a4.b4@gmail.com', now(), now()  ),
-(5, 'a5', 'b5', 'a5.b5@gmail.com', now(), now()  ),
-(6, 'a6', 'b6', 'a6.b6@gmail.com', now(), now()  ),
-(7, 'a7', 'b7', 'a7.b7@gmail.com', now(), now()  ),
-(8, 'a8', 'b8', 'a8.b8@gmail.com', now(), now()  ),
-(9, 'a9', 'b9', 'a9.b9@gmail.com', now(), now()  ),
-(10, 'a10', 'b10', 'a10.b10@gmail.com', now(), now()  ),
-(11, 'a11', 'b11', 'a11.b11@gmail.com', now(), now()  ),
-(12, 'a12', 'b12', 'a12.b12@gmail.com', now(), now()  ),
-(13, 'a13', 'b13', 'a13.b13@gmail.com', now(), now()  ),
-(14, 'a14', 'b14', 'a14.b14@gmail.com', now(), now()  ),
-(15, 'a15', 'b15', 'a15.b15@gmail.com', now(), now()  ),
-(16, 'a16', 'b16', 'a16.b16@gmail.com', now(), now()  ),
-(17, 'a17', 'b17', 'a17.b17@gmail.com', now(), now()  ),
-(18, 'a18', 'b18', 'a18.b18@gmail.com', now(), now()  ),
-(19, 'a19', 'b19', 'a19.b19@gmail.com', now(), now()  ),
-(20, 'a20', 'b20', 'a20.b20@gmail.com', now(), now()  ),
-(21, 'a21', 'b21', 'a21.b21@gmail.com', now(), now()  ),
-(22, 'a22', 'b22', 'a22.b22@gmail.com', now(), now()  ),
-(23, 'a23', 'b23', 'a23.b23@gmail.com', now(), now()  );
-
+values 
+('f4e37960-5823-4f40-983e-5158b1a0465c', 'a1', 'b1', 'a1.b1@gmail.com', now(), now()  ),
+('02d8daa4-ba6e-4b41-a644-207fd3546a8f', 'a2', 'b2', 'a2.b2@gmail.com', now(), now()  ),
+('d9a657d3-7da7-4fbe-88af-6bdcd5d2cb95', 'a3', 'b3', 'a3.b3h@gmail.com',now(), now()  ),
+('f110fc74-e018-44ff-ba96-e70f71956f14', 'a4', 'c4', 'a4.b4@gmail.com', now(), now()  ),
+('d30a175e-5cd3-41c0-bf4b-5186b86c2839', 'a5', 'b5', 'a5.b5@gmail.com', now(), now()  ),
+('1004877c-026d-4248-a4af-ff107a924085', 'a6', 'b6', 'a6.b6@gmail.com', now(), now()  ),
+('7a3c7337-7582-47d0-9316-ca2b76f77bed', 'a7', 'b7', 'a7.b7@gmail.com', now(), now()  ),
+('a18e36d4-9623-48d0-b372-3d11f97f7f08', 'a8', 'b8', 'a8.b8@gmail.com', now(), now()  ),
+('e7d568bb-c8d5-49b6-acc3-24528a72284a', 'a9', 'b9', 'a9.b9@gmail.com', now(), now()  ),
+('dd123230-a8cc-4654-90b9-94ef998f7ea8', 'a10', 'b10', 'a10.b10@gmail.com', now(), now()  ),
+('7f59ed11-2014-45d1-921b-b8d5c5210c71', 'a11', 'b11', 'a11.b11@gmail.com', now(), now()  ),
+('480e2fde-5a80-4940-9b1f-1ef1ac342c61', 'a12', 'b12', 'a12.b12@gmail.com', now(), now()  ),
+('f9205a90-857e-43af-be37-515f7d2c2e59', 'a13', 'b13', 'a13.b13@gmail.com', now(), now()  ),
+('98585ff5-e340-42ed-861d-b55b19bde43b', 'a14', 'b14', 'a14.b14@gmail.com', now(), now()  ),
+('c57908e3-3f68-4da6-94b2-f7af38127e82', 'a15', 'b15', 'a15.b15@gmail.com', now(), now()  ),
+('f0261709-14d9-484a-aa3e-825c9aa512d4', 'a16', 'b16', 'a16.b16@gmail.com', now(), now()  ),
+('0029d6fc-31c7-4038-92e7-b05629bb66ca', 'a17', 'b17', 'a17.b17@gmail.com', now(), now()  ),
+('e4b65099-46d6-4e40-92b7-e512601012e6', 'a18', 'b18', 'a18.b18@gmail.com', now(), now()  ),
+('2ccc0424-992e-4aa5-9ab5-ee094b118352', 'a19', 'b19', 'a19.b19@gmail.com', now(), now()  ),
+('10c1b296-577c-4956-a60b-2668f3f4e4e3', 'a20', 'b20', 'a20.b20@gmail.com', now(), now()  ),
+('d62db554-17fe-4125-aafd-b9b7654c58b7', 'a21', 'b21', 'a21.b21@gmail.com', now(), now()  ),
+('363097c9-9626-4ce4-98a0-3949787213e0', 'a22', 'b22', 'a22.b22@gmail.com', now(), now()  ),
+('5549ad56-f560-457b-a594-e4f2a10836c6', 'a23', 'b23', 'a23.b23@gmail.com', now(), now()  );
 
 
 insert into courses (id, name, description, created_at, updated_at)
 values 
-(1, 'English For Beginners Group1', 'Engish for Beginners', now(), now()),
-(2, 'English For Beginners Group2', 'Engish for Beginners', now(), now()),
-(3, 'English For Beginners Group3', 'Engish for Beginners', now(), now()),
-(4, 'English For Beginners Group4', 'Engish for Beginners', now(), now()),
-(5, 'IELTS G1', 'IELTS Engish for IELTS', now(), now()),
-(6, 'IELTS G2', 'IELTS', now(), now()),
-(7, 'IELTS G3', 'IELTS', now(), now()),
-(8, 'IELTS G4', 'IELTS', now(), now()),
-(9, 'Math G1', 'math', now(), now()),
-(10, 'Math G2', 'math', now(), now()),
-(11, 'Math G3', 'math', now(), now()),
-(12, 'Math G4', 'math', now(), now()),
-(13, 'Math G5', 'math', now(), now()),
-(14, 'Math G6', 'math', now(), now()),
-(15, 'Math G7', 'math', now(), now());
+('81372e72-ffb0-4054-9c57-47924872e1e6', 'English For Beginners Group1', 'Engish for Beginners', now(), now()),
+('deaf113a-f2e1-49e0-8976-f2bb401ea3a8', 'English For Beginners Group2', 'Engish for Beginners', now(), now()),
+('7a470c9e-830d-4e55-8bee-cf49cde85486', 'English For Beginners Group3', 'Engish for Beginners', now(), now()),
+('ac6a5f17-fcc6-40d9-aa24-92a8217c3fa6', 'English For Beginners Group4', 'Engish for Beginners', now(), now()),
+('94063351-a513-44d7-88fe-65bc1e29b795', 'IELTS G1', 'IELTS Engish for IELTS', now(), now()),
+('e012f6ed-ab53-4b09-bd5b-c352c05f7922', 'IELTS G2', 'IELTS', now(), now()),
+('28428ecf-6cf0-4901-af51-948c3524b260', 'IELTS G3', 'IELTS', now(), now()),
+('6111e365-381d-4801-a976-b3b4c6bf2dd1', 'IELTS G4', 'IELTS', now(), now()),
+('8d287eea-038b-4610-a7db-7b00f7b03fea', 'Math G1', 'math', now(), now()),
+('ae8d5774-7db7-4742-8d7b-5c81faae867e', 'Math G2', 'math', now(), now()),
+('026a9d18-f2e8-4db3-95f9-bb03cc203406', 'Math G3', 'math', now(), now()),
+('4ba46f9e-9a31-4745-81e9-c697c65a8c5c', 'Math G4', 'math', now(), now()),
+('be0ee516-7579-4c86-ba18-f54c62c7c78a', 'Math G5', 'math', now(), now()),
+('f2a4aed6-d178-480d-aa42-41a350bf1673', 'Math G6', 'math', now(), now()),
+('0e00053d-84af-41e7-ad8b-60f2b0d243cc', 'Math G7', 'math', now(), now());
                                              
