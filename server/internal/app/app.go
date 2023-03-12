b8ebf028cca5617231e69c445ad671fe0c0eb7fd
My Name <you@example.com>
2023-03-12 22:51:12 +0600

    created App

--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -1,1 +1,27 @@
 package app
+
+import (
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/repos"
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/services"
+)
+
+type App struct {
+	// repos
+	CentresRepo repos.CentresRepo
+
+	// services
+	CentresService services.CentresService
+
+	MethodNames map[string]string
+}
+
+func NewApp(
+	centresRepo repos.CentresRepo,
+	centresService services.CentresService,
+	methodNames map[string]string) *App {
+	return &App{
+		CentresRepo:    centresRepo,
+		CentresService: centresService,
+		MethodNames:    methodNames,
+	}
+}
