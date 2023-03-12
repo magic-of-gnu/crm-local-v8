65ff9ea71c9bfff4230fabb1e3f8396116a00b0f
My Name <you@example.com>
2023-03-12 22:53:03 +0600

    added CreateOne method

--- server/internal/services/centres.go
+++ server/internal/services/centres.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
 	"github.com/magic-of-gnu/crm-local-v8/server/internal/repos"
 )
@@ -17,4 +20,22 @@ func NewCentresService(centresRepo repos.CentresRepo) *centresService {
 
 func (ss *centresService) GetAll() ([]models.Centre, error) {
 	return ss.CentresRepo.GetAll()
+}
+
+func (ss *centresService) CreateOne(name, description string) (*models.Centre, error) {
+	var centre *models.Centre
+
+	uid, err := uuid.NewRandom()
+	if err != nil {
+		return centre, err
+	}
+
+	time_now := time.Now()
+
+	centre, err = ss.CentresRepo.CreateOne(uid, name, description, time_now, time_now)
+	if err != nil {
+		return centre, err
+	}
+
+	return centre, nil
 }
