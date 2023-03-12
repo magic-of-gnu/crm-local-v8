946573d029fd4f946cadbb866bc49308b5c8b40a
My Name <you@example.com>
2023-03-12 22:52:32 +0600

    added new method

--- server/internal/services/services.go
+++ server/internal/services/services.go
@@ -1,7 +1,10 @@
 package services
 
-import "github.com/magic-of-gnu/crm-local-v8/server/internal/models"
+import (
+	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
+)
 
 type CentresService interface {
 	GetAll() ([]models.Centre, error)
+	CreateOne(name, description string) (*models.Centre, error)
 }
