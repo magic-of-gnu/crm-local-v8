b6080c1d5ed38cccbf545e21c8adc1eac5dcd468
My Name <you@example.com>
2023-04-02 17:29:11 +0600

    added service interfaces

--- server/internal/services/services.go
+++ server/internal/services/services.go
@@ -1,10 +1,48 @@
 package services
 
 import (
+	"github.com/google/uuid"
 	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
 )
 
 type CentresService interface {
 	GetAll() ([]models.Centre, error)
 	CreateOne(name, description string) (*models.Centre, error)
+}
+
+type RoomsService interface {
+	GetAll() ([]models.Room, error)
+	CreateOne(
+		centre_uid uuid.UUID,
+		name string,
+		num_seats int,
+		info string,
+	) (*models.Room, error)
+}
+
+type EmployeesService interface {
+	GetAll() ([]models.Employee, error)
+	CreateOne(
+		first_name,
+		last_name,
+		username,
+		info string,
+	) (*models.Employee, error)
+}
+
+type StudentsService interface {
+	GetAll() ([]models.Student, error)
+	CreateOne(
+		first_name,
+		last_name,
+		username string,
+	) (*models.Student, error)
+}
+
+type CoursesService interface {
+	GetAll() ([]models.Course, error)
+	CreateOne(
+		name,
+		description string,
+	) (*models.Course, error)
 }
