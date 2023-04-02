48adc8a999124cc0aff6b53028a48bc7ceeaef1f
My Name <you@example.com>
2023-04-02 17:14:12 +0600

    added repo interfaces

--- server/internal/repos/repo.go
+++ server/internal/repos/repo.go
@@ -11,3 +11,48 @@ type CentresRepo interface {
 	GetAll() ([]models.Centre, error)
 	CreateOne(id uuid.UUID, name, description string, created_at, updated_at time.Time) (*models.Centre, error)
 }
+
+type RoomsRepo interface {
+	GetAll() ([]models.Room, error)
+	CreateOne(
+		uid,
+		centre_uid uuid.UUID,
+		name string,
+		num_seats int,
+		info string,
+		created_at, updated_at time.Time,
+	) (*models.Room, error)
+}
+
+type EmployeesRepo interface {
+	GetAll() ([]models.Employee, error)
+	CreateOne(
+		uid uuid.UUID,
+		first_name,
+		last_name,
+		username,
+		info string,
+		created_at, updated_at time.Time,
+	) (*models.Employee, error)
+}
+
+type StudentsRepo interface {
+	GetAll() ([]models.Student, error)
+	CreateOne(
+		uid uuid.UUID,
+		first_name,
+		last_name,
+		username string,
+		created_at, updated_at time.Time,
+	) (*models.Student, error)
+}
+
+type CoursesRepo interface {
+	GetAll() ([]models.Course, error)
+	CreateOne(
+		uid uuid.UUID,
+		name,
+		description string,
+		created_at, updated_at time.Time,
+	) (*models.Course, error)
+}
