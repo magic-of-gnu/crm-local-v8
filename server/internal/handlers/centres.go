cacdf4fbfb089f72bf418ee58108775b6499f631
My Name <you@example.com>
2023-04-02 17:30:21 +0600

    added handlers for centres

--- server/internal/handlers/centres.go
+++ server/internal/handlers/centres.go
@@ -3,13 +3,52 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
 )
 
+func GetAllCentres(c *gin.Context) {
+
+	fmt.Println("binding start")
+	centres, err := App.CentresService.GetAll()
+	fmt.Println("binding finished; centres: ", centres)
+
+	if err != nil {
+
+		c.JSON(http.StatusOK, gin.H{
+			"title": "Centres List",
+			"error": err.Error(),
+		})
+
+		return
+	}
+
+	data := make(map[string]interface{})
+	centres_data := make([]map[string]string, len(centres))
+
+	for ii, centre := range centres {
+		centres_data[ii] = map[string]string{
+			"id":          centre.ID.String(),
+			"name":        centre.Name,
+			"description": centre.Description,
+			"created_at":  centre.CreatedAt.Format(time.DateTime),
+			"updated_at":  centre.UpdatedAt.Format(time.DateTime),
+		}
+	}
+	data["data"] = centres_data
+	fmt.Println("here")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": "Centres List",
+		"data":  data,
+	})
+}
+
 func GetCentres(c *gin.Context) {
-	c.HTML(http.StatusOK, "centres.html", gin.H{
+
+	c.JSON(http.StatusOK, gin.H{
 		"title":       "Centres",
 		"post_action": "centres",
 	})
@@ -20,7 +59,7 @@ func PostCentres(c *gin.Context) {
 	var centre *models.Centre
 	if err := c.Bind(&centre); err != nil {
 		fmt.Println("error: ", err)
-		c.HTML(http.StatusInternalServerError, "centres.html", gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"title": "Centres",
 		})
 		return
@@ -29,13 +68,13 @@ func PostCentres(c *gin.Context) {
 	centre, err := App.CentresService.CreateOne(centre.Name, centre.Description)
 	if err != nil {
 		fmt.Println("error: ", err)
-		c.HTML(http.StatusInternalServerError, "centres.html", gin.H{
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"title": "Centres",
 		})
 		return
 	}
 
-	c.HTML(http.StatusOK, "centres.html", gin.H{
+	c.JSON(http.StatusOK, gin.H{
 		"title": "Centres",
 	})
 }
