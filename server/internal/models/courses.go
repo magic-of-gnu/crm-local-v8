88eee4f825867f18a7cd7b0a38cb87e29b3e0735
My Name <you@example.com>
2023-04-02 17:14:43 +0600

    added model for courses

--- server/internal/models/courses.go
+++ server/internal/models/courses.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Course struct {
-	ID          uuid.UUID
-	Name        string
-	Description string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID          uuid.UUID `json:"id" validate:"omitempty,uuid4"`
+	Name        string    `json:"name" validate:"required,min=3,max=50"`
+	Description string    `json:"description" validate:"omitempty"`
+	CreatedAt   time.Time `json:"created_at" validate:"omitempty,datetime"`
+	UpdatedAt   time.Time `json:"update_at" validate:"omitempty,datetime"`
 }
