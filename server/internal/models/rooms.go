11bd086cd0d54a8374f403182dcce8b22e3424bb
My Name <you@example.com>
2023-04-02 17:15:20 +0600

    added model for rooms

--- server/internal/models/rooms.go
+++ server/internal/models/rooms.go
@@ -8,10 +8,10 @@ import (
 
 type Room struct {
 	ID        uuid.UUID
-	CentreID  uuid.UUID
-	Name      string
-	NumSeats  int
-	Info      string
+	CentreID  uuid.UUID `json:"centre_id"`
+	Name      string    `json:"name"`
+	NumSeats  int       `json:"num_seats"`
+	Info      string    `json:"info"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
