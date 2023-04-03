a4afdbb8a2a87bb2474c68170ecf6046c3758bc2
My Name <you@example.com>
2023-04-04 01:25:32 +0600

    added request and response for student_courses

--- server/internal/models/student_courses.go
+++ server/internal/models/student_courses.go
@@ -7,11 +7,37 @@ import (
 )
 
 type StudentCourses struct {
-	ID            uuid.UUID
-	StudentID     uuid.UUID
-	CourseID      uuid.UUID
-	PaymentAmount int
-	Description   string
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
+	ID            uuid.UUID `json:"id" validate:"omitempty,uuid4_rfc4122"`
+	StudentID     uuid.UUID `json:"student_id" validate:"required"`
+	CourseID      uuid.UUID `json:"course_id" validate:"required"`
+	PaymentAmount int       `json:"payment_amount" validate:"required,number"`
+	Description   string    `json:"description" validate:"omitempty"`
+	CreatedAt     time.Time `json:"created_at" validate:"omitempty,datetime"`
+	UpdatedAt     time.Time `json:"updated_at" validate:"omitempty,datetime"`
+	// StudentName   string    `json:"student_name" validate:"ignore"`
+	Student Student
+	Course  Course
+}
+
+type StudentCoursesListResponse struct {
+	ID               uuid.UUID `json:"id"`
+	StudentID        uuid.UUID `json:"student_id"`
+	CourseID         uuid.UUID `json:"course_id"`
+	PaymentAmount    int       `json:"payment_amount"`
+	Description      string    `json:"description"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	StudentFirstName string    `json:"student_first_name"`
+	StudentLastName  string    `json:"student_last_name"`
+	CourseName       string    `json:"course_name"`
+}
+
+type StudentCoursesRequest struct {
+	ID            uuid.UUID `json:"id" validate:"omitempty,uuid4_rfc4122"`
+	StudentID     uuid.UUID `json:"student_id" validate:"required"`
+	CourseID      uuid.UUID `json:"course_id" validate:"required"`
+	PaymentAmount int       `json:"payment_amount" validate:"required,number"`
+	Description   string    `json:"description" validate:"omitempty"`
+	CreatedAt     time.Time `json:"created_at" validate:"omitempty,datetime"`
+	UpdatedAt     time.Time `json:"updated_at" validate:"omitempty,datetime"`
 }
