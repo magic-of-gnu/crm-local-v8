9e816017c6ba95f1b83672550399075787414d60
My Name <you@example.com>
2023-04-03 10:57:08 +0600

    added create one view
    added create one view for attendance_values

---
+++ web/src/views/custom/views/attendance_values/AttendanceValuesCreateOneView.vue
@@ -0,0 +1,63 @@
+<template>
+  <CRow>
+    <CForm action="some-post-function" method="POST">
+      <div class="mb-3">
+        <CFormLabel for="value">Value</CFormLabel>
+        <CFormInput
+          v-model.number="value"
+          type="text"
+          id="value"
+          name="value"
+          placeholder="Enter Course value (has to be a number)"
+        />
+      </div>
+      <div class="mb-3">
+        <CFormLabel for="first_name">Name</CFormLabel>
+        <CFormInput
+          v-model.trim="name"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter Course Name"
+        />
+      </div>
+      <div class="mb-3">
+        <CFormLabel for="description">Description</CFormLabel>
+        <CFormInput
+          v-model.trim="description"
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Enter Description"
+        />
+      </div>
+      <CButton
+        @click.prevent="($event) => run_this_method($event)"
+        component="input"
+        type="submit"
+        color="primary"
+        value="Submit"
+      />
+    </CForm>
+  </CRow>
+</template>
+
+<script setup>
+/* eslint-disable */
+
+import m from '@/views/custom/hooks/attendanceValues/methods.js'
+import { ref } from 'vue'
+
+const value = ref("")
+const name = ref("")
+const description = ref("")
+
+function run_this_method(event) {
+  m.postCreateOne({
+    value: value.value,
+    name: name.value,
+    description: description.value,
+  })
+}
+
+</script>
