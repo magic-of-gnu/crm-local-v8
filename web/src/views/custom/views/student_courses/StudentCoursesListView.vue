f24ee62601e46ffb37d79253a7e52f848090556d
My Name <you@example.com>
2023-04-04 01:28:12 +0600

    added list view for student_courses

---
+++ web/src/views/custom/views/student_courses/StudentCoursesListView.vue
@@ -0,0 +1,64 @@
+<template>
+  <CRow>
+    <CCol :xs="12">
+      <CCard class="mb-4">
+        <CCardHeader>
+          <CRow class="justify-content-between">
+            <CCol>
+              <strong>Vue Progress</strong> <small>Basic example</small>
+            </CCol>
+            <CCol class="align-self-end">
+              <router-link
+                :to="{ name: 'student_courses_create_one' }"
+                component="CButton"
+                color="primary"
+                disabled
+                >Create One
+              </router-link>
+            </CCol>
+          </CRow>
+        </CCardHeader>
+        <CCardBody>
+          <CTable hover>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">ID</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Student Name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Course Name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Payment Amount</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Created At</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Updated At</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Operations</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow v-for="item in itemsList" :key="item.id">
+                <CTableDataCell>{{ item.id }}</CTableDataCell>
+                <CTableDataCell>{{ item.student_first_name }} {{ item.student_last_name }}</CTableDataCell>
+                <CTableDataCell>{{ item.course_name }}</CTableDataCell>                
+                <CTableDataCell>{{ item.payment_amount }}</CTableDataCell>
+                <CTableDataCell>{{ item.description }}</CTableDataCell>
+                <CTableDataCell>{{ item.created_at }}</CTableDataCell>
+                <CTableDataCell>{{ item.updated_at }}</CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script setup>
+import m from '@/views/custom/hooks/studentCourses/methods.js'
+import { ref, onMounted } from 'vue'
+
+const itemsList = ref('')
+
+onMounted(() => {
+  m.getAllList().then((d) => {
+    itemsList.value = d.data.data
+  })
+})
+</script>
