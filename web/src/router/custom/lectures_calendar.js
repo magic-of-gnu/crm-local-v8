ad22a8349c7f242033f10f8ca60b39c7913d8666
My Name <you@example.com>
2023-04-07 12:50:37 +0600

    added routes for lectures_calendar

---
+++ web/src/router/custom/lectures_calendar.js
@@ -0,0 +1,21 @@
+const routes = {
+  path: '/lectures_calendar',
+  name: 'LecturesCalendar List',
+  redirect: '/lectures_calendar/list',
+  children: [
+    {
+      path: '/lectures_calendar/create_one',
+      name: 'lectures_calendar_create_one',
+      component: () =>
+        import('@/views/custom/views/lectures_calendar/LecturesCalendarCreateOneView.vue'),
+    },
+    {
+      path: '/lectures_calendar/list',
+      name: 'lectures_calendar_list',
+      component: () => 
+        import('@/views/custom/views/lectures_calendar/LecturesCalendarListView.vue'),
+    },
+  ],
+}
+
+export default routes;
