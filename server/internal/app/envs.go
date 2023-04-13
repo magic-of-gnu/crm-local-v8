f3469ba169274e1f22685eb516194d8a1a8b443d
My Name <you@example.com>
2023-04-13 14:26:49 +0600

    added paths to keys

--- server/internal/app/envs.go
+++ server/internal/app/envs.go
@@ -9,13 +9,15 @@ import (
 // var DATABASE_URL string = "dbname=local_v8 user=postgres password=postgres host=127.0.0.1 port=5432"
 
 type AppConfig struct {
-	Port        string
-	PG_Host     string
-	PG_Port     string
-	PG_DBName   string
-	PG_User     string
-	PG_Password string
-	HashCost    int
+	Port               string
+	PG_Host            string
+	PG_Port            string
+	PG_DBName          string
+	PG_User            string
+	PG_Password        string
+	HashCost           int
+	PublicKeyPath      string
+	PrivateKeyPath     string
 }
 
 func NewAppConfig() (*AppConfig, error) {
@@ -26,14 +28,12 @@ func NewAppConfig() (*AppConfig, error) {
 		return &AppConfig{}, fmt.Errorf("error during creating AppConfig, could not read HASH_COST, either empty or non int; err: %w", err)
 	}
 
+	// public/private keys
+	private_key_path := os.Getenv("PRIVATE_KEY_PATH")
+	public_key_path := os.Getenv("PUBLIC_KEY_PATH")
 	return &AppConfig{
-		Port:        port,
-		PG_Host:     os.Getenv("PG_HOST"),
-		PG_Port:     os.Getenv("PG_PORT"),
-		PG_DBName:   os.Getenv("PG_DBNAME"),
-		PG_User:     os.Getenv("PG_USER"),
-		PG_Password: os.Getenv("PG_PASSWORD"),
-		HashCost:    hash_cost,
+		PublicKeyPath:      public_key_path,
+		PrivateKeyPath:     private_key_path,
 	}, nil
 }
 
