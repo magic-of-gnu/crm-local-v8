d82fa63d81188cb59d7b330363e4e235dcb7aaa9
My Name <you@example.com>
2023-04-14 19:07:49 +0600

    added token restoration from local storage

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,13 @@
   <router-view />
 </template>
 
+<script setup>
+import store from '@/store/index.js'
+
+store.dispatch('tryLogin')
+
+</script>
+
 <style lang="scss">
 // Import Main styles for this application
 @import 'styles/style';
