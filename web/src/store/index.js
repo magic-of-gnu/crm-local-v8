dd850be13ef5153b3c594569ffa9d616733743a2
My Name <you@example.com>
2023-04-14 19:03:13 +0600

    added auth module

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import authModule from './modules/auth/index.js'
 
 export default createStore({
   state: {
@@ -17,5 +18,7 @@ export default createStore({
     },
   },
   actions: {},
-  modules: {},
+  modules: {
+    auth: authModule,
+  },
 })
