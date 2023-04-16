53a479ec0641d374aeb0ff89a9d33c9865945143
My Name <you@example.com>
2023-04-17 00:11:10 +0600

    added search methods

--- server/internal/repos/lecture_calendar.go
+++ server/internal/repos/lecture_calendar.go
@@ -119,7 +119,6 @@ func (rr *lectureCalendarPostgresRepo) CreateOne(
 	)
 
 	if err != nil {
-		fmt.Println("err: ", err)
 		return &item, err
 	}
 
@@ -148,15 +147,143 @@ func (rr *lectureCalendarPostgresRepo) DeleteOneByID(uid uuid.UUID) error {
 	row, err := rr.dbpool.Exec(ctx, query, uid)
 
 	if err != nil {
-		fmt.Println("err: ", err)
 		return err
 	}
 
 	if row.RowsAffected() == 0 {
-		fmt.Println("err: data was not deleted")
 		return fmt.Errorf("data was not deleted")
 	}
 
 	return nil
+
+}
+
+func (rr *lectureCalendarPostgresRepo) GetManyByCourseID(course_id uuid.UUID) ([]models.LectureCalendar, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	var items []models.LectureCalendar
+
+	query := `select a.id, a.room_id, a.course_id, a.employee_id, a.date, a.duration, a.created_at, a.updated_at, 
+	   b.centre_id, b.name, b.num_seats, b.info, b.created_at, b.updated_at, 
+	   c.name, c.description, c.created_at, c.updated_at,
+	   d.first_name, d.last_name, d.username, d.info, d.created_at, d.updated_at
+	   from lectures_calendar a
+left join rooms b on a.room_id  = b.id
+left join courses c on a.course_id = c.id
+left join employees d on a.employee_id = d.id
+WHERE course_id = $1`
+
+	rows, err := rr.dbpool.Query(ctx, query, course_id)
+	defer rows.Close()
+
+	if err != nil {
+		return items, err
+	}
+
+	for rows.Next() {
+		var item models.LectureCalendar
+		err = rows.Scan(
+			&item.ID,
+			&item.RoomID,
+			&item.CourseID,
+			&item.EmployeeID,
+			&item.Date,
+			&item.Duration,
+			&item.CreatedAt,
+			&item.UpdatedAt,
+			&item.Room.CentreID,
+			&item.Room.Name,
+			&item.Room.NumSeats,
+			&item.Room.Info,
+			&item.Room.CreatedAt,
+			&item.Room.UpdatedAt,
+			&item.Course.Name,
+			&item.Course.Description,
+			&item.Course.CreatedAt,
+			&item.Course.UpdatedAt,
+			&item.Employee.FirstName,
+			&item.Employee.LastName,
+			&item.Employee.Username,
+			&item.Employee.Info,
+			&item.Employee.CreatedAt,
+			&item.Employee.UpdatedAt,
+		)
+
+		if err != nil {
+			return items, err
+		}
+
+		item.Room.ID = item.RoomID
+		item.Course.ID = item.CourseID
+		item.Employee.ID = item.EmployeeID
+
+		items = append(items, item)
+	}
+
+	if err = rows.Err(); err != nil {
+		return items, err
+	}
+
+	return items, nil
+
+}
+
+func (rr *lectureCalendarPostgresRepo) GetOneByID(uid uuid.UUID) (*models.LectureCalendar, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	var item models.LectureCalendar
+
+	query := `select a.id, a.room_id, a.course_id, a.employee_id, a.date, a.duration, a.created_at, a.updated_at, 
+	   b.centre_id, b.name, b.num_seats, b.info, b.created_at, b.updated_at, 
+	   c.name, c.description, c.created_at, c.updated_at,
+	   d.first_name, d.last_name, d.username, d.info, d.created_at, d.updated_at
+	   from lectures_calendar a
+left join rooms b on a.room_id  = b.id
+left join courses c on a.course_id = c.id
+left join employees d on a.employee_id = d.id
+WHERE a.id = $1`
+
+	row := rr.dbpool.QueryRow(ctx, query, uid)
+
+	err := row.Scan(
+		&item.ID,
+		&item.RoomID,
+		&item.CourseID,
+		&item.EmployeeID,
+		&item.Date,
+		&item.Duration,
+		&item.CreatedAt,
+		&item.UpdatedAt,
+		&item.Room.CentreID,
+		&item.Room.Name,
+		&item.Room.NumSeats,
+		&item.Room.Info,
+		&item.Room.CreatedAt,
+		&item.Room.UpdatedAt,
+		&item.Course.Name,
+		&item.Course.Description,
+		&item.Course.CreatedAt,
+		&item.Course.UpdatedAt,
+		&item.Employee.FirstName,
+		&item.Employee.LastName,
+		&item.Employee.Username,
+		&item.Employee.Info,
+		&item.Employee.CreatedAt,
+		&item.Employee.UpdatedAt,
+	)
+
+	if err != nil {
+		return &item, err
+	}
+
+	item.Room.ID = item.RoomID
+	item.Course.ID = item.CourseID
+	item.Employee.ID = item.EmployeeID
+
+	return &item, nil
 
 }
