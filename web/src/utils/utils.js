00f09850f5921bec8b0807de595c49ce776979f0
My Name <you@example.com>
2023-04-28 01:43:40 +0600

    added new methods

--- web/src/utils/utils.js
+++ web/src/utils/utils.js
@@ -18,9 +18,39 @@ function utcTimeToTimeHoursMinutes(time_string) {
   return date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0')
 }
 
+function utcTimeToUpdateTime(time_string) {
+  const date = new Date(time_string);
+  const [yyyy, month, day] = [date.getFullYear(), date.toLocaleString('default', { month: 'long' }), date.getDate()]
+  const [hh, mm, ss] = [date.getHours().toString().padStart(2, '0'), date.getMinutes().toString().padStart(2, '0'), date.getSeconds().toString().padStart(2, '0')]
+  return `${yyyy} ${month} ${day} ${hh}:${mm}:${ss}`
+}
+
+function showUUID(uuid_str, l=4) {
+  return uuid_str.slice(0, l)
+}
+
+function sortList(data, col, asc) {
+  if (asc[col]) {
+    data.sort((x, y) => (x[col] > y[col] ? -1 : 1));
+  } else {
+    data.sort((x, y) => (x[col] < y[col] ? -1 : 1));
+  }
+  console.log("before asc[col]: ", asc[col])
+  asc[col] = !asc[col]
+  console.log("after asc[col]: ", asc[col])
+}
+
+function stringOperators() {return ["in", "=", "!="]}
+function numberOperators() {return ["=", "!=", ">", "<", ">=", "<="]}
+
 export {
   epochToDatetime,
   utcTimeToDate,
   utcTimeToLocaleTime,
   utcTimeToTimeHoursMinutes,
+  utcTimeToUpdateTime,
+  sortList,
+  stringOperators,
+  numberOperators,
+  showUUID,
 }
