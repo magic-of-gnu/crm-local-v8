5ef43fa3d3d1d7204ff7127d899ff2b74724c0d7
My Name <you@example.com>
2023-04-28 01:43:23 +0600

    added ToastComponent

---
+++ web/src/components/ToastComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<CToaster placement="top-end" visible>
+  <CToast
+    v-for="(toast, index) in props.toasts"
+    :key="index"
+    :color="toast.color"
+    visible="true"
+  >
+    <div class="d-flex">
+      <CToastBody>
+        {{ toast.content }}
+      </CToastBody>  
+      <CToastClose class="me-2 m-auto"/>
+    </div>
+  </CToast>
+</CToaster>
+
+</template>
+
+<script setup>
+import { CToast, CToaster, CToastBody, CToastClose } from '@coreui/vue'
+const props = defineProps(['toasts'])
+</script>
