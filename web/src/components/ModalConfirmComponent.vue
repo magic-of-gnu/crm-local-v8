6f66179b7a59c19fff066789e2b2a4350ad821b0
My Name <you@example.com>
2023-04-28 15:17:14 +0600

    added modal template

---
+++ web/src/components/ModalConfirmComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<CModal 
+  alignment="center" 
+  backdrop="static"
+  :visible="props.showModal"
+>
+  <CModalBody>
+    {{ props.content }}
+  </CModalBody>
+  <CModalFooter>
+    <CButton color="secondary" @click="props.confirmDeleteClick(false)">
+      {{ props.closeBtnText }}
+    </CButton>
+    <CButton color="primary" @click="props.confirmDeleteClick(true)">
+      {{ props.confirmBtnText }}
+    </CButton>
+  </CModalFooter>
+</CModal>
+</template>
+
+<script setup>
+const props = defineProps([
+  "content",
+  "closeBtnText",
+  "confirmBtnText",
+  "showModal",
+  "confirmDeleteClick"
+])
+</script>
