1440046261c79f7521d61641bafad39b2a761f3a
My Name <you@example.com>
2023-05-01 22:50:49 +0600

    refactor

--- web/src/views/custom/views/attendance_values/AttendanceValuesListView.vue
+++ web/src/views/custom/views/attendance_values/AttendanceValuesListView.vue
@@ -1,4 +1,73 @@
 <template>
+<ToastComponent 
+  v-if="toasts"
+  :toasts="toasts"
+/>
+
+<ConfirmModal 
+  content="Please, Confirm that You Want to Delete"
+  closeBtnText="Cancel"
+  confirmBtnText="Confirm"
+  :showModal="showModal"
+  :confirmDeleteClick="confirmDeleteClick"
+/>
+
+  <CRow>
+    <CCol xs="12">
+    <CForm @submit.prevent>
+
+      <NumFilter 
+        dataType="number"
+        inputName="value"
+        inputTitle="Value"
+        v-model:inputModel="searchValues[0]"
+        inputPlaceholder="Enter Value"
+        opName="value_op"
+        opTitle="Operation"
+        v-model:opModel="searchOps[0]"
+        :opOptions="valueSearchOpOptions"
+      />
+
+      <StringFilter 
+        inputName="name"
+        inputTitle="Name"
+        v-model:inputModel="searchValues[0]"
+        inputPlaceholder="Enter Name"
+        opName="name_op"
+        opTitle="Operation"
+        v-model:opModel="searchOps[0]"
+        :opOptions="nameSearchOpOptions"
+      />
+
+      <StringFilter 
+        inputName="description"
+        inputTitle="Description"
+        v-model:inputModel="searchValues[1]"
+        inputPlaceholder="Enter Description Filter"
+        opName="description_op"
+        opTitle="Operation"
+        v-model:opModel="searchOps[1]"
+        :opOptions="descriptionSearchOpOptions"
+      />
+
+      <CRow class="mb-3">
+        <CCol>
+          <CButton
+            @click.prevent="localApplyFilter"
+            component="button"
+            type="button"
+            color="primary"
+          >Search</CButton>
+        </CCol>
+      </CRow>
+
+    </CForm>
+    </CCol>
+  </CRow>
+
+
+
+
   <CRow>
     <CCol :xs="12">
       <CCard class="mb-4">
@@ -33,12 +102,12 @@
             </CTableHead>
             <CTableBody>
               <CTableRow v-for="item in itemsList" :key="item.id">
-                <CTableDataCell>{{ item.id }}</CTableDataCell>
+                <CTableDataCell>{{ showUUID(item.id) }}</CTableDataCell>
                 <CTableDataCell>{{ item.value }}</CTableDataCell>                
                 <CTableDataCell>{{ item.name }}</CTableDataCell>
                 <CTableDataCell>{{ item.description }}</CTableDataCell>
-                <CTableDataCell>{{ item.created_at }}</CTableDataCell>
-                <CTableDataCell>{{ item.updated_at }}</CTableDataCell>
+                <CTableDataCell>{{ utcTimeToUpdateTime(item.created_at) }}</CTableDataCell>
+                <CTableDataCell>{{ utcTimeToUpdateTime(item.updated_at) }}</CTableDataCell>
               </CTableRow>
             </CTableBody>
           </CTable>
@@ -50,13 +119,87 @@
 
 <script setup>
 import m from '@/views/custom/hooks/attendanceValues/methods.js'
-import { ref, onMounted } from 'vue'
+import { ref, onBeforeMount, reactive } from 'vue'
+import { sortList, stringOperators, numberOperators, showUUID } from '@/utils/utils.js'
+import { utcTimeToUpdateTime } from '@/utils/utils.js'
+import StringFilter from '@/components/custom_filters/StringFilter.vue'
+import NumFilter from '@/components/custom_filters/StringFilter.vue'
+import ToastComponent from '@/components/ToastComponent.vue'
+import { populateToastsFromResponse } from '@/utils/toasts';
+import ConfirmModal from '@/components/ModalConfirmComponent.vue'
+import { applyFilter } from '@/utils/filter.js'
+
+const showModal = ref(false)
+const toasts = ref([])
+
+const valueSearchOpOptions = numberOperators().map((x) => {
+  return {label: x, value: x}
+})
+const nameSearchOpOptions = stringOperators().map((x) => {
+  return {label: x, value: x}
+})
+const descriptionSearchOpOptions = stringOperators().map((x) => {
+  return {label: x, value: x}
+})
+const createdAtSearchOpOptions = numberOperators().map((x) => {
+  return {label: x, value: x}
+})
+const updatedAtSearchOpOptions = numberOperators().map((x) => {
+  return {label: x, value: x}
+})
+const originalData = ref(null)
+const columnNames = ["value", "name", "description", "created_at", "updated_at"]
+const selectedItems = ref({})
+
+const searchValues = ref(new Array(columnNames.length).fill(null))
+const searchOps = ref([
+  valueSearchOpOptions[0].value,
+  nameSearchOpOptions[0].value,
+  descriptionSearchOpOptions[0].value,
+  createdAtSearchOpOptions[0].value,
+  updatedAtSearchOpOptions[0].value,
+])
 
 const itemsList = ref('')
-
-onMounted(() => {
-  m.getAllList().then((d) => {
-    itemsList.value = d.data.data
-  })
+let sort_asc = reactive({
+  value: true,
+  name: true,
+  description: true,
+  created_at: true,
+  updated_at: true,
+  id: true,
 })
+
+function confirmDeleteOneByID(id) {
+  showModal.value = true
+  selectedItems.value[id] = true
+}
+
+async function confirmDeleteClick(btnValue) {
+  if (btnValue === true) {
+    let response = null
+    for (const key in selectedItems.value) {
+        response = await m.postDeleteOneByID(key)
+      }
+    populateToastsFromResponse(response, toasts)
+    await reset()
+  }
+  selectedItems.value = {}
+  showModal.value = false
+}
+
+function localApplyFilter() {
+  applyFilter(originalData.value, columnNames, searchValues.value, searchOps.value, itemsList)
+}
+
+async function reset() {
+  const response = await m.getAllList()
+  originalData.value = response.data.data.data
+  itemsList.value = originalData.value.slice()
+}
+
+onBeforeMount(async () => {
+  await reset()
+})
+
 </script>
