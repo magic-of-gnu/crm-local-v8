8fa341ec99861db42dcc79afdea97342db7e3c21
My Name <you@example.com>
2023-05-01 22:28:13 +0600

    added json tags

--- server/internal/models/employees.go
+++ server/internal/models/employees.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Employee struct {
-	ID        uuid.UUID
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Username  string `json:"username"`
-	Info      string `json:"info"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uuid.UUID `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Username  string    `json:"username"`
+	Info      string    `json:"info"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
