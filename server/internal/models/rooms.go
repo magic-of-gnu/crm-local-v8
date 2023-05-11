3d84e3d9d39c0076ed0525a5a6b9bd974cd956e8
My Name <you@example.com>
2023-05-11 12:18:08 +0600

    added json tags to rooms

--- server/internal/models/rooms.go
+++ server/internal/models/rooms.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Room struct {
-	ID        uuid.UUID
+	ID        uuid.UUID `json:"id"`
 	CentreID  uuid.UUID `json:"centre_id"`
 	Name      string    `json:"name"`
 	NumSeats  int       `json:"num_seats"`
 	Info      string    `json:"info"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
