9017011ff92ee02b093252c00a2fd74484e08e94
My Name <you@example.com>
2023-05-11 14:31:24 +0600

    added methods to courses repo

--- server/internal/repos/courses.go
+++ server/internal/repos/courses.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/georgysavva/scany/v2/pgxscan"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/magic-of-gnu/crm-local-v8/server/internal/models"
 )
@@ -76,5 +77,78 @@ func (rr *coursesPostgresRepo) CreateOne(
 	}
 
 	return &item, nil
+
+}
+
+func (rr *coursesPostgresRepo) DeleteOneByID(uid uuid.UUID) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	query := "DELETE FROM courses WHERE id = $1"
+
+	row, err := rr.dbpool.Exec(ctx, query, uid)
+
+	if err != nil {
+		return err
+	}
+
+	if row.RowsAffected() == 0 {
+		return fmt.Errorf("data was not deleted")
+	}
+
+	return nil
+
+}
+
+func (rr *coursesPostgresRepo) GetOneByID(uid uuid.UUID) (*models.Course, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	query := `SELECT id, name, description, created_at, updated_at FROM courses
+	where id = $1`
+
+	var result []models.Course
+	err := pgxscan.Select(ctx, rr.dbpool, &result, query, uid)
+
+	if err == pgx.ErrNoRows {
+		return &models.Course{}, nil
+	}
+
+	if err != nil {
+		return &result[0], err
+	}
+
+	return &result[0], nil
+}
+
+func (rr *coursesPostgresRepo) UpdateOneByID(createItem *models.Course) error {
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
+	query := `UPDATE courses SET
+	name = $2,
+	description = $3,
+	updated_at = $4
+	WHERE id = $1
+	`
+
+	row, err := rr.dbpool.Exec(ctx, query,
+		&createItem.ID,
+		&createItem.Name,
+		&createItem.Description,
+		&createItem.UpdatedAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	if row.RowsAffected() == 0 {
+		return fmt.Errorf("data was not updated")
+	}
+
+	return nil
 
 }
